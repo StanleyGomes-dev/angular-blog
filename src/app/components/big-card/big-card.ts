51ba63df337afc991471e271d19b07dd90c7aead
Stanley <stangomes3@gmail.com>
2025-08-01 01:34:18 -0300

    componets dinamycs propertys

--- src/app/components/big-card/big-card.ts
+++ src/app/components/big-card/big-card.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './big-card.css'
 })
 export class BigCard {
-
+  @Input()
+  photoCover: string = ""
+  @Input()
+  cardTitle: string = ""
+  @Input()
+  cardDescription: string= ""
 }
